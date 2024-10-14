c8f74e259c3dbbea27fcc23a2e8b95d5a1926f88
Matheus Jatczak Bertazzo <matheus@matheusbertazzo.com>
2024-10-14 00:48:00 -0300

    Fixed LSP hotkeys

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,9 +6,9 @@ require("mason").setup()
 --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 
 require('cmp').setup {
-  sources = {
-    { name = 'nvim_lsp' }
-  }
+	sources = {
+		{ name = 'nvim_lsp' }
+	}
 }
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -33,134 +33,134 @@ local servers = {
 require("mason-lspconfig").setup {
 	ensure_installed = vim.tbl_keys(servers or {}),
 	handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
+		function(server_name)
+			local server = servers[server_name] or {}
 
-	    -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end
-        },
+			-- This handles overriding only values explicitly passed
+			-- by the server configuration above. Useful when disabling
+			-- certain features of an LSP (for example, turning off formatting for ts_ls)
+			server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+			require('lspconfig')[server_name].setup(server)
+		end
+	},
 }
 
 -- LSP Auto commands
--- TODO: Setup kickstart auto commands
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = {buffer = true}
-      vim.keymap.set(mode, lhs, rhs, opts)
-    end
+	desc = 'LSP actions',
+	group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			mode = mode or 'n'
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+		end
 
-    -- Displays hover information about the symbol under the cursor
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+		-- Jump to the definition
+		map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
-    -- Jump to the definition
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+		-- Lists all the implementations for the symbol under the cursor
+		map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementations')
 
-    -- Jump to declaration
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+		-- Fuzzy find all the symbols in your current document.
+		map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
-    -- Lists all the implementations for the symbol under the cursor
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+		-- Fuzzy find all the symbols in your current workspace.
+		map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-    -- Jumps to the definition of the type symbol
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+		-- Lists all the references
+		map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
-    -- Lists all the references 
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+		-- Displays hover information about the symbol under the cursor
+		map('K', vim.lsp.buf.hover, 'Displays hover information for the symbol under the cursor')
 
-    -- Displays a function's signature information
-    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+		-- Renames all references to the symbol under the cursor
+		map('<F2>', vim.lsp.buf.rename, 'Rename symbol')
 
-    -- Renames all references to the symbol under the cursor
-    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+		-- Selects a code action available at the current cursor position
+		map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
-    -- Selects a code action available at the current cursor position
-    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+		-- Move to the previous diagnostic
+		map('[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', '[G]oto []')
 
-    -- Show diagnostics in a floating window
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+		-- Move to the next diagnostic
+		map(']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', '[G]oto []')
 
-    -- Move to the previous diagnostic
-    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+		-- Displays a function's signature information
+		map('gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', '[G]oto function [S]ignature')
 
-    -- Move to the next diagnostic
-    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  end
+		-- Jump to declaration. Note this is not definition, it'll take you to the header of the file for most languages.
+		map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+	end
 })
 
 -- Snippets engine
 require('luasnip.loaders.from_vscode').lazy_load()
 
 -- Autocomplete configuration
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 local cmp = require('cmp')
 local luasnip = require('luasnip')
-local select_opts = {behavior = cmp.SelectBehavior.Select}
+local select_opts = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
-  },
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp', keyword_length = 1},
-    {name = 'buffer', keyword_length = 3},
-    {name = 'luasnip', keyword_length = 2},
-  },
-  window = {
-    documentation = cmp.config.window.bordered()
-  },
-  formatting = {
-    fields = {'menu', 'abbr', 'kind'},
-    format = function(entry, item)
-      local menu_icon = {
-        nvim_lsp = 'λ',
-        luasnip = '⋗',
-        buffer = 'Ω',
-        path = '🖫',
-      }
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end
+	},
+	sources = {
+		{ name = 'path' },
+		{ name = 'nvim_lsp', keyword_length = 1 },
+		{ name = 'buffer',   keyword_length = 3 },
+		{ name = 'luasnip',  keyword_length = 2 },
+	},
+	window = {
+		documentation = cmp.config.window.bordered()
+	},
+	formatting = {
+		fields = { 'menu', 'abbr', 'kind' },
+		format = function(entry, item)
+			local menu_icon = {
+				nvim_lsp = 'λ',
+				luasnip = '⋗',
+				buffer = 'Ω',
+				path = '🖫',
+			}
 
-      item.menu = menu_icon[entry.source.name]
-      return item
-    end,
-  },
-  mapping = {
-    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+			item.menu = menu_icon[entry.source.name]
+			return item
+		end,
+	},
+	mapping = {
+		['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+		['<Down>'] = cmp.mapping.select_next_item(select_opts),
 
-    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+		['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+		['<C-n>'] = cmp.mapping.select_next_item(select_opts),
 
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp.mapping.confirm({select = true}),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
 
-    ['<C-f>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
+		['<C-f>'] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
 
-    ['<C-b>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  },
+		['<C-b>'] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	},
 })
